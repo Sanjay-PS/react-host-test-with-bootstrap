220f40fb41d6292adf84ad612dd8cf88ba4796f4
Anonymous <Anonymous@xyz.com>
2020-07-28 00:15:54 +0530

    Updates

---
+++ precache-manifest.27986973b8646e29243a9184e39b9e5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba55b2d5a49cc2a24f4d5fb624385fff",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/index.html"
+  },
+  {
+    "revision": "2ee084e720427cd88044",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bd93824bf87ed0e6c0a6",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/css/main.db221cc4.chunk.css"
+  },
+  {
+    "revision": "2ee084e720427cd88044",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/js/2.ded70359.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/js/2.ded70359.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd93824bf87ed0e6c0a6",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/js/main.3d1dcf58.chunk.js"
+  },
+  {
+    "revision": "00c5e035f3cf3e323dcc",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/js/runtime-main.cef90eb6.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/Sanjay-PS/react-host-test-with-bootstrap/static/media/logo.ee7cd8ed.svg"
+  }
+]);
